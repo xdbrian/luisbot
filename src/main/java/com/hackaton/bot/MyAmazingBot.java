4bb38d94bef4481cd58b970d49bf86ec5882abc9
S65037 <btorrey@bcp.com.pe>
2018-07-26 22:48:09 -0500

    Agrega funcionalidad para guardar funcionario y retornar informacion

--- src/main/java/com/hackaton/bot/MyAmazingBot.java
+++ src/main/java/com/hackaton/bot/MyAmazingBot.java
@@ -1,6 +1,8 @@
 package com.hackaton.bot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
@@ -12,12 +14,27 @@ import org.telegram.telegrambots.exceptions.TelegramApiException;
  */
 public class MyAmazingBot extends TelegramLongPollingBot {
 
+
+  @Autowired
+  Funtionary funtionary;
+
   @Override
   public void onUpdateReceived(Update update) {
+
+    SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+            .setChatId(update.getMessage().getChatId());
+
     if (update.hasMessage() && update.getMessage().hasText()) {
-      SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-              .setChatId(update.getMessage().getChatId())
-              .setText(update.getMessage().getText());
+      final Message msn = update.getMessage();
+
+      if (msn.getText().startsWith("/soybcp")){
+        funtionary.initBcp(msn,message);
+      }else  if(funtionary.getSaveFuntionary()){
+        funtionary.saveFuntionary(msn,message);
+      }else  if(funtionary.getSavePassword()) {
+        funtionary.savePassword(msn,message);
+      }
+
       try {
         execute(message); // Call method to send the message
       } catch (TelegramApiException e) {
@@ -29,12 +46,12 @@ public class MyAmazingBot extends TelegramLongPollingBot {
   @Override
   public String getBotUsername() {
     // TODO
-    return "Luis21Bot";
+    return "Brian";
   }
 
   @Override
   public String getBotToken() {
-    return "680599289:AAF9mRhmMiSWxyFjZqQIaREljaiWwtcDsXE";
+    return "686336894:AAEAlhqRqmg9JEJ0WIhuM6AY5VYkBr4O7VQ";
   }
 
 }
